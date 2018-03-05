3f6588dc68c69f8eba074171b0e3784d479b86b5
mohy <engmohyeldean@gmail.com>
2018-03-05 10:33:27 +0200

    return contacts

--- src/providers/contacts/contacts.ts
+++ src/providers/contacts/contacts.ts
@@ -1,7 +1,7 @@
-import { Storage } from '@ionic/storage';
-import { Contacts } from '@ionic-native/contacts';
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import {Storage} from '@ionic/storage';
+import {Contacts} from '@ionic-native/contacts';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
 
 /*
   Generated class for the ContactsProvider provider.
@@ -12,21 +12,30 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ContactsProvider {
   constructor(public http: HttpClient,
-  public contacts:Contacts, private storage: Storage,) {
+              public contacts: Contacts, private storage: Storage,) {
     console.log('Hello ContactsProvider Provider');
     this.getcontacts();
   }
- getcontacts ():any {
-  this.storage.get('contacts').then(val=>{
-    return val
-  })
+
+  getcontacts(): any {
+    this.storage.get('contacts').then(val => {
+      alert("inside get from storage" + val)
+      return val
+    })
     // alert('initialze Items');
-    this.contacts.find(['displayName', 'name', 'phoneNumbers', 'emails'], {filter: "", multiple: true,hasPhoneNumber:true}).then((contacts) => {
-      this.storage.set('contacts',contacts);
+    this.contacts.find(['displayName', 'name', 'phoneNumbers', 'emails'], {
+      filter: "",
+      multiple: true,
+      hasPhoneNumber: true
+    }).then((contacts) => {
+      alert("contacts ===" + contacts)
+
+      this.storage.set('contacts', contacts);
+      return contacts
     }, err => {
-      return(err);
+      return (err);
     });
   }
- 
+
 }
 
